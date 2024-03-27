8708bd1ed42eeede822cf5c41a2017326d1aa522
Agenc0 <tangentskate174@gmail.com>
2024-03-27 17:16:04 +0000

    added t3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argnum = sys.argv
+    arglen = len(argnum)
+    argsum = 0
+
+    if arglen > 1:
+        for i in range(1, arglen)
+        argsum += int(argnum[i])
+    print(argsum)
