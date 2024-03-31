941dfaac64f5f56c78c626788143fc93874aa4ef
Agenc0 <tangenskate174@gmail.com>
2024-03-31 04:23:24 +0000

    overhauled code

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
+    if type(roman_string) != str or roman_string == "None":
         return 0
-
-        def assigner(letter):
-            ronumbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                    'C': 100, 'D': 500, 'M': 1000}
-            if letter not in ronumbers:
+    else:
+        def roman_converter(r):
+            roman_numbers = {
+                    'I': 1,
+                    'V': 5,
+                    'X': 10,
+                    'L': 50,
+                    'C': 100,
+                    'D': 500,
+                    'M': 1000}
+            if r not in roman_numbers:
                 return -1
             else:
-                return ronumbers[letter]
+                return roman_numbers[r]
 
-        res, i = 0
+        result = 0
+        i = 0
         while i < len(roman_string):
-            inum = assigner(roman_string[i])
+            curr_num = roman_converter(roman_string[i])
 
             if i + 1 < len(roman_string):
-                next_num = assigner(roman_string[i + 1])
-                if inum >= next_num:
-                    res += inum
+                next_num = roman_converter(roman_string[i + 1])
+                if curr_num >= next_num:
+                    result += curr_num
                     i += 1
                 else:
-                    res += next_num - inum
+                    result += next_num - curr_num
                     i += 2
             else:
-                res += inum
+                result += curr_num
                 i += 1
 
-        return res
+        return result
