f45e19394e5e447169885c50fe24a184364a95e5
Agenc0 <tangenskate174@gmail.com>
2024-03-31 03:02:32 +0000

    added t0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    newmatrix = []
+    for i in matrix:
+        res = list(map(lambda n: n * n, i))
+        newmatrix.append(res)
+        return newmatrix
---
+++ python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+directory for python, more data structures project
