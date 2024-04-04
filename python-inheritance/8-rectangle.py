bea0451b45f100697f7cc0ab7e9f683831b03593
Agenc0 <tangenskate174@gmail.com>
2024-04-04 04:35:08 +0000

    fixed pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """defines class Rectangle"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
