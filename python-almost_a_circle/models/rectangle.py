a963fb56e18807484c2ce017ff2c768effa2efb4
Agenc0 <tangenskate174@gmail.com>
2024-04-07 01:23:23 +0000

    added t7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,7 +76,10 @@ class Rectangle(Base):
 
     def display(self):
         """print Rectangle with #"""
+        for i in range(self.__y):
+            print("")
         for i in range(self.__height):
+            print(" " * self.__x, end="")
             print("#" * self.__width)
 
     def __str__(self):
