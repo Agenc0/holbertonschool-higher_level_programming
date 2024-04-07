34657646947fa9732c29f2716415c5807f5d267d
Agenc0 <tangenskate174@gmail.com>
2024-04-07 02:08:29 +0000

    added t18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -37,3 +37,14 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if dictionary != {}:
+            if cls.__name__ == "Square":
+                dummy = cls(1)
+            else:
+                dummy = cls(1, 1)
+
+            dummy.update(**dictionary)
+            return dummy
